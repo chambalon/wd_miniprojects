e1e3ef8753cef57a6420f354b5c59f00f65d05ae
chambalon <nehachambalonk@gmail.com>
2025-07-09 17:59:49 +0800

    commit2

--- to-do list/script.js
+++ to-do list/script.js
@@ -1,8 +1,9 @@
 const todoText = document.getElementById('todo-text'),
   addUpdateClick = document.getElementById('addupdateclick'),
-  listItems = document.getElementById('list-items');  
+  listItems = document.getElementById('list-items'),
+  alertMessage = document.getElementById('alertmessage');  
 let updateText;
-let todoData = JSON.parse(localStorage.getItem('todoData'));
+let todoData = JSON.parse(localStorage.getItem('todo-Data'));
 if(!todoData){
   todoData = [];
 }
@@ -14,7 +15,47 @@ todoText.addEventListener('keypress',function(e){
 });
 
 
-ReadToDoList();
+function CreateToDoList(){
+  SetAlertMessage("");
+  if(todoText.value === "") {
+    SetAlertMessage("Please enter your text");
+    todoText.focus();
+  }else{
+  let li = document.createElement('li');
+  const listItem = `<div ondblclick="CompleteToDoList(this)">${todoText.value}</div> 
+  <div><img class="edit todo-controls" onclick="UpdateToDoList(this)" src="edit.svg">
+  <img class="delete todo-controls" onclick="DeleteToDoList(this)" src="delete.svg"></div>`;
+  li.innerHTML = listItem;
+  listItems.appendChild(li);
+
+  if(!todoData){
+    todoData = [];
+  }
+  let todoItem = {item:todoText.value, status:false};
+  todoData.push(todoItem);
+  SetLocalStorage()
+
+  todoText.value = ''; 
+  SetAlertMessage("New task created successfully");
+  }
+}
+
+
+function CompleteToDoList(e) {
+  if(e.parentElement.querySelector('div').style.textDecoration === ''){
+    e.parentElement.querySelector('div').style.textDecoration = 'line-through';
+    e.parentElement.querySelector('img.edit').remove();
+  }
+
+  todoData.forEach(element =>{
+    if(e.parentElement.querySelector('div').innerText == element.item){
+      element.status = true;
+    }
+  });
+
+  SetAlertMessage("Task completed successfully");
+}
+
 function ReadToDoList(){
   todoData.forEach(element => {
     let li = document.createElement('li');
@@ -23,78 +64,49 @@ function ReadToDoList(){
       style = "style = 'textDecoration line-through'";
     }
     const listItem = `<div ${style} ondblclick="CompleteToDoList(this)">${element.item}${style === ""? 
-      '<img class="edit todo-controls" onclick="UpdateToDoList(this)">':""} 
-      <img class="edit todo-controls" onclick="UpdateToDoList(this)"></div>`;
+      '<img class="edit todo-controls" onclick="UpdateToDoList(this)" src="edit.svg">':""} 
+      <img class="delete todo-controls" onclick="DeleteToDoList(this)" src="delete.svg"></div>`;
     li.innerHTML = listItem;
     listItems.appendChild(li);
   });
 }
-
-
-function CreateToDoList(){
-  if(todoText.value === ''){
-    alert("Please enter your text");
-    todoText.focus();
-  }
-    const listItem = `<div ondblclick="CompleteToDoList(this)">${todoText.value}</div> 
-    <div><img class="edit todo-controls" onclick="UpdateToDoList(this)" src="edit_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg">
-    <img class="delete todo-controls" onclick="DeleteToDoList(this)" src="delete_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"></div>`;
-    let li = document.createElement('li');
-    li.innerHTML = listItem;
-    listItems.appendChild(li);
-
-    if(!todoData){
-      todoData = [];
-    }
-    let todoItem = {item:todoText.value, status:false};
-    todoData.push(todoItem);
-   
-
-    todoText.value = ''; 
-}
-
-
-function CompleteToDoList(e) {
-  if(e.parentElement.querySelector('div').style.textDecoration === ''){
-    e.parentElement.querySelector('div').style.textDecoration = 'line-through';
-  }
-
-  todoData.forEach(element =>{
-    if(e.parentElement.querySelector('div').innerText == element.item){
-      element.status = true;
-    }
-  });
-}
+ReadToDoList();
 
 
 function UpdateOnSelection() {
-  updateText.innerText = todoText.value;
-  addUpdateClick.setAttribute('onclick','CompleteToDoList()');
-  addUpdateClick.setAttribute('src','add_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg');
-  todoText.value =''; 
-
   todoData.forEach(element => {
     if(element.item == updateText.innerText) {
       element.item = todoText.value;
     }
   });
+
+  SetLocalStorage();
+
+  updateText.innerText = todoText.value;
+  addUpdateClick.setAttribute('onclick','CreateToDoList()');
+  addUpdateClick.setAttribute('src','add.svg');
+  todoText.value =''; 
+  SetAlertMessage("To-do list updated successfully");
 }
 
 
 function UpdateToDoList(e) {
   if(e.parentElement.parentElement.querySelector('div').style.textDecoration === ''){
-    todoText.value = e.parentElement.parentElement.querySelector('div').innerHTML;
+    todoText.value = e.parentElement.parentElement.querySelector('div').innerText;
     addUpdateClick.setAttribute('onclick','UpdateOnSelection()');
-    addUpdateClick.setAttribute('src','refresh_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg');
+    addUpdateClick.setAttribute('src','refresh.svg');
     updateText = e.parentElement.parentElement.querySelector('div');
+    todoText.focus();
   }
 }
 
 
 function DeleteToDoList(e) {
   let deleteList = e.parentElement.parentElement.querySelector('div').innerText;
+
   if(confirm(`Are you sure, do you want to delete ${deleteList}?`)){
-    e.parentElement.parentElement.parentElement.querySelector('li').remove();
+    e.parentElement.parentElement.remove();
+    e.parentElement.parentElement.setAttribute('class','deleted-item');
     todoText.focus();
 
     todoData.forEach(element =>{
@@ -103,9 +115,23 @@ function DeleteToDoList(e) {
       }
     });
     SetLocalStorage();
+    SetAlertMessage("To-do list deleted successfully");
+
   }
 }
 
 function SetLocalStorage(){
-  localStorage.setItem('todoData', JSON.stringify(todoData));
+  localStorage.setItem('todo-Data', JSON.stringify(todoData));
+}
+
+
+localStorage.clear();
+
+function SetAlertMessage(message){
+  alertMessage.removeAttribute('class');
+  alertMessage.innerText = message;
+
+  setTimeout(()=>{
+    alertMessage.classList.add('toggle');
+  }, 1000);
 }
